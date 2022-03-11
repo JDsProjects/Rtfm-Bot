5613297a558ce6ca82f3dab84639876a1e427f28
JDJG Inc. Official <ghostzap123@gmail.com>
2022-03-10 22:38:52 -0500

    please just work.

--- cogs/rtfm_slash.py
+++ cogs/rtfm_slash.py
@@ -7,6 +7,11 @@ class test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @app_commands.command(description="looks up docs")
+    async def rtfm(interaction: discord.Interaction, library: str):
+        await interaction.response.send_message(f"Alright Let's see {library}")
+
+    @rtfm.autocomplete()
     async def rtfm_autocomplete(
         interaction: discord.Interaction, current: str, namespace: discord.AppCommandOptionType.string
     ):
@@ -17,11 +22,6 @@ class test(commands.Cog):
             for library in rtfm
             if current.lower() in library.lower()
         ]
-
-    @app_commands.command(description="looks up docs")
-    @app_commands.autocomplete(library=rtfm_autocomplete)
-    async def rtfm(interaction: discord.Interaction, library: str):
-        await interaction.response.send_message(f"Alright Let's see {library}")
 
 
 def setup(bot):
