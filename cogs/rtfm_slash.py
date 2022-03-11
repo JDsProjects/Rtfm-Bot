a96d1bbb8c93e92338e1ec82719e152f4182d000
JDJG Inc. Official <ghostzap123@gmail.com>
2022-03-10 23:17:03 -0500

    let's see

--- cogs/rtfm_slash.py
+++ cogs/rtfm_slash.py
@@ -8,10 +8,22 @@ class test(commands.Cog):
         self.bot = bot
 
     @app_commands.command(description="looks up docs")
-    async def rtfm(self, interaction: discord.Interaction, library: str):
+    async def rtfm(self, interaction: discord.Interaction, library: str, query: str):
         await interaction.response.send_message(f"Alright Let's see {library}")
 
     @rtfm.autocomplete("library")
+    async def rtfm_autocomplete(
+        self, interaction: discord.Interaction, current: str, namespace: discord.AppCommandOptionType.string
+    ):
+        rtfm = interaction.client.rtfm_libraries
+
+        return [
+            app_commands.Choice(name=f"{library}", value=f"{rtfm.get(library)}")
+            for library in rtfm
+            if current.lower() in library.lower()
+        ]
+
+    @rtfm.autocomplete("query")
     async def rtfm_autocomplete(
         self, interaction: discord.Interaction, current: str, namespace: discord.AppCommandOptionType.string
     ):
