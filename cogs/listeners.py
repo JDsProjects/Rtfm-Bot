a2fdc5a2cc22e681b58cb43a5932c4be6085aac9
JDJG Inc. Official <ghostzap123@gmail.com>
2022-05-15 13:37:24 -0400

    Revert "Revert "Reformat bot and add typings""

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -1,25 +1,31 @@
+from typing import TYPE_CHECKING
 from discord.ext import commands
-import discord, random, os
 
+if TYPE_CHECKING:
+    from main import RTFMBot
+else:
+    RTFMBot = commands.Bot
 
 class Events(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: RTFMBot) -> None:
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_ready(self):
-        print("Bot is Ready")
-        print(f"Logged in as {self.bot.user}")
-        print(f"Id: {self.bot.user.id}")
+    async def on_ready(self) -> None:
+        print(
+            "Bot is Ready",
+            f"Logged in as {self.bot.user} (ID: {self.bot.user.id})",  # type: ignore # .user isn't None.
+            sep="\n"
+        )
 
     @commands.Cog.listener()
-    async def on_guild_available(self, guild):
-        print(f"{guild} is avaible")
+    async def on_guild_available(self, guild) -> None:
+        print(f"Guild {guild.name} (ID: {guild.id}) is available")
 
     @commands.Cog.listener()
-    async def on_guild_unavailable(self, guild):
-        print(f"{guild} is unavaible")
+    async def on_guild_unavailable(self, guild) -> None:
+        print(f"Guild {guild.name} (ID: {guild.id}) is unavailable")
 
 
-async def setup(bot):
+async def setup(bot: RTFMBot) -> None:
     await bot.add_cog(Events(bot))
--- cogs/rtfm_slash.py
+++ cogs/rtfm_slash.py
@@ -1,59 +1,62 @@
-import discord
-from discord import app_commands
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from discord import Object
+
 from discord.ext import commands
 import typing
+from discord.app_commands import AppCommandError, command as app_command, Choice
+
+if TYPE_CHECKING:
+    from main import RTFMBot
+
+    from discord import Interaction
+else:
+    RTFMBot = commands.Bot
 
 
-class test(commands.Cog):
-    def __init__(self, bot):
+class RTFMSlash(commands.Cog):
+    def __init__(self, bot: RTFMBot) -> None:
         self.bot = bot
 
-    @app_commands.command(description="looks up docs")
-    async def rtfm(self, interaction: discord.Interaction, library: str, query: typing.Optional[str] = None):
-
+    @app_command(description="looks up docs")
+    async def rtfm(self, interaction: Interaction, library: str, query: typing.Optional[str] = None) -> None:
+        """Looks up docs for a library with optionally a query."""
         if query is None:
             return await interaction.response.send_message(f"Alright Let's see {library}")
 
         await interaction.response.send_message(f"Alright Let's see {library}{query}")
 
     @rtfm.autocomplete("library")
-    async def rtfm_autocomplete(self, interaction: discord.Interaction, current: str):
-        rtfm = interaction.client.rtfm_libraries
+    async def rtfm_library_autocomplete(self, interaction: Interaction, current: str) -> list[Choice]:
+        libraries = self.bot.rtfm_libraries
 
         if not current:
+            return [Choice(name=name, value=link) for name, link in libraries.items()][0:25]
 
-            return [app_commands.Choice(name=f"{library}", value=f"{rtfm.get(library)}") for library in rtfm][0:25]
-        return [
-            app_commands.Choice(name=f"{library}", value=f"{rtfm.get(library)}")
-            for library in rtfm
-            if current.lower() in library.lower()
+        return [Choice(name=name, value=link) for name, link in libraries.items() if current.lower() in name.lower()][
+            0:25
         ]
 
     @rtfm.autocomplete("query")
-    async def rtfm_autocomplete(
-        self,
-        interaction: discord.Interaction,
-        current: str,
-    ):
-        default = list(interaction.client.rtfm_libraries.values())[0]
-        url = interaction.namespace.library or default
+    async def rtfm_query_autocomplete(self, interaction: Interaction, current: str) -> list[Choice]:
+        url = interaction.namespace.library or list(self.bot.rtfm_libraries.values())[0]
 
+        assert self.bot.scraper is not None
         results = await self.bot.scraper.search(current, page=url)
 
-        if not results:
-            results = {"Not Found", f"{current}."}
+        if not results or not current:
+            results = ["Not Found", f"{current}."]
+            return [Choice(name=item, value=item) for item in results]
 
         results = results[:25]
-        results = dict(results)
-        cut_off = len(url)
-        return [app_commands.Choice(name=f"{result}", value=f"{results.get(result)[cut_off:]}") for result in results]
+        return [Choice(name=name, value=link[len(url) :]) for name, link in results][0:25]
 
     @rtfm.error
-    async def rtfm_error(self, interaction: discord.Interaction, command, error):
+    async def rtfm_error(self, interaction: Interaction, error: AppCommandError) -> None:
         await interaction.response.send_message(f"{error}! Please Send to this to my developer", ephemeral=True)
         print(error)
-        print(command)
+        print(interaction.command)
 
 
-async def setup(bot):
-    await bot.add_cog(test(bot))
+async def setup(bot: RTFMBot) -> None:
+    await bot.add_cog(RTFMSlash(bot), guild=Object(423828791098605578))
