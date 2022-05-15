1fe1756b91bc8be2967877d2efc742130b5396f5
Soheab <33902984+Soheab@users.noreply.github.com>
2022-05-15 21:12:19 +0200

    fix autocomplete and others

--- cogs/rtfm_slash.py
+++ cogs/rtfm_slash.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-from discord import Object
 
 from discord.ext import commands
 import typing
@@ -21,7 +20,7 @@ class RTFMSlash(commands.Cog):
     @app_command(description="looks up docs")
     async def rtfm(self, interaction: Interaction, library: str, query: typing.Optional[str] = None) -> None:
         """Looks up docs for a library with optionally a query."""
-        if query is None:
+        if query is None or query == "No Results Found":
             return await interaction.response.send_message(f"Alright Let's see {library}")
 
         await interaction.response.send_message(f"Alright Let's see {library}{query}")
@@ -30,12 +29,12 @@ class RTFMSlash(commands.Cog):
     async def rtfm_library_autocomplete(self, interaction: Interaction, current: str) -> list[Choice]:
         libraries = self.bot.rtfm_libraries
 
-        if not current:
-            return [Choice(name=name, value=link) for name, link in libraries.items()][0:25]
+        all_choices: list[Choice] = [Choice(name=name, value=link) for name, link in libraries.items()]
+        startswith: list[Choice] = [choices for choices in all_choices if choices.name.startswith(current)]
+        if not (current and startswith):
+            return all_choices[0:25]
 
-        return [Choice(name=name, value=link) for name, link in libraries.items() if current.lower() in name.lower()][
-            0:25
-        ]
+        return startswith
 
     @rtfm.autocomplete("query")
     async def rtfm_query_autocomplete(self, interaction: Interaction, current: str) -> list[Choice]:
@@ -44,12 +43,16 @@ class RTFMSlash(commands.Cog):
         assert self.bot.scraper is not None
         results = await self.bot.scraper.search(current, page=url)
 
-        if not results or not current:
-            results = ["Not Found", f"{current}."]
-            return [Choice(name=item, value=item) for item in results]
+        if not results:
+            return [Choice(name="No results found", value="No Results Found")]
 
-        results = results[:25]
-        return [Choice(name=name, value=link[len(url) :]) for name, link in results][0:25]
+        to_slice_link = len(url)
+        all_choices: list[Choice] = [Choice(name=name, value=link[to_slice_link:]) for name, link in results]
+        startswith: list[Choice] = [choices for choices in all_choices if choices.name.startswith(current)]
+        if not current:
+            return all_choices[:25]
+
+        return startswith[:25]
 
     @rtfm.error
     async def rtfm_error(self, interaction: Interaction, error: AppCommandError) -> None:
