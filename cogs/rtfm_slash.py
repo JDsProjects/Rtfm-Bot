f45ba1a498eb87cee932857430bd0270840ff34a
JDJG <ghostzap123@gmail.com>
2024-03-19 02:26:29 -0400

    There

--- cogs/rtfm_slash.py
+++ cogs/rtfm_slash.py
@@ -65,6 +65,50 @@ class RTFMSlash(commands.Cog):
         print(error)
         print(interaction.command)
 
+    @app_commands.private_channel_only()
+    @app_commands.command(description="looks up docs but for dms and group chats", name="rtfm-private")
+    async def rtfm_slash_private(
+        self, interaction: discord.Interaction, library: str, query: typing.Optional[str] = None
+    ) -> None:
+        """Looks up docs for a library with optionally a query."""
+        if query is None or query == "No Results Found":
+            return await interaction.response.send_message(f"Alright Let's see \n{library}")
+
+        await interaction.response.send_message(f"Alright Let's see \n{library+query}")
+
+    @rtfm_slash_private.autocomplete("library")
+    async def rtfm_library_private_autocomplete(self, interaction: discord.Interaction, current: str) -> list[Choice]:
+        libraries = self.bot.rtfm_libraries
+
+        all_choices: list[Choice] = [Choice(name=name, value=link) for name, link in libraries.items()]
+        startswith: list[Choice] = [choices for choices in all_choices if choices.name.startswith(current)]
+        if not (current and startswith):
+            return all_choices[0:25]
+
+        return startswith[0:25]
+
+    @rtfm_slash_private.autocomplete("query")
+    async def rtfm_query_private_autocomplete(self, interaction: discord.Interaction, current: str) -> list[Choice]:
+        url = interaction.namespace.library or list(dict(self.rtfm_dictionary).values())[0]
+        unfiltered_results = await utils.rtfm(self.bot, url)
+
+        all_choices = [Choice(name=result.name, value=result.url.replace(url, "")) for result in unfiltered_results]
+
+        if not current:
+            return all_choices[:25]
+
+        filtered_results = fuzzy.finder(current, unfiltered_results, key=lambda t: t[0])
+
+        results = [Choice(name=result.name, value=result.url.replace(url, "")) for result in filtered_results]
+
+        return results[:25]
+
+    @rtfm_slash.error
+    async def rtfm_error(self, interaction: discord.Interaction, error) -> None:
+        await interaction.response.send_message(f"{error}! Please Send to this to my developer", ephemeral=True)
+        print(error)
+        print(interaction.command)
+
 
 async def setup(bot: RTFMBot) -> None:
     await bot.add_cog(RTFMSlash(bot))
