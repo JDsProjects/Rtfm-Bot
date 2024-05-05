80f0a9b76888409071600f6353d564a732a091cb
JDJG <ghostzap123@gmail.com>
2024-05-05 07:12:40 -0400

    Updates a few files:

--- utils/extra.py
+++ utils/extra.py
@@ -128,4 +128,3 @@ async def algolia_lookup(bot: RTFMBot, app_id: str, app_key: str, index: str, qu
             results.append(RtfmObject(value["anchor"], value["url"]))
 
         return results
-      
